26c525a2a83d258a7762b2c74c7739d2e856661c
vanda <konovalovavanda@gmail.com>
2019-01-07 17:59:29 +0300

    Инициализация репозитория.

---
+++ Lab9/UsingLoadFrom/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace UsingLoadFrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly privateAss = Assembly.LoadFrom("SimpleAssembly.dll");
+            MethodInfo info = privateAss.GetTypes()[0].GetMethod("HelloWorld");
+            Object obj = info.Invoke(null, null);
+            Console.WriteLine("Результат выполнения метода: {0}", obj);
+            Console.Read();
+        }
+    }
+}
